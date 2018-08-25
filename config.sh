3f7467ca240756364d9636fc8dd506f667209cb3
Nathaniel J. Smith <njs@pobox.com>
2018-08-24 19:42:43 -0700

    what is going on

--- config.sh
+++ config.sh
@@ -4,6 +4,9 @@
 
 function run_tests {
     set -x
+    pwd
+    ls
+    env
     # On Linux, multibuild runs this test inside a docker container, so it
     # doesn't have access to the envvars we set in the main .travis.yml.
     eval $(python ./mb.py build_spec_to_shell build-spec.json)
