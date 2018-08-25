27987917fd274f5ba773879561a2b1c089b9c984
Nathaniel J. Smith <njs@pobox.com>
2018-08-24 19:36:12 -0700

    Reload envvars inside the test docker

--- config.sh
+++ config.sh
@@ -3,6 +3,10 @@
 # }
 
 function run_tests {
+    set -x
+    # On Linux, multibuild runs this test inside a docker container, so it
+    # doesn't have access to the envvars we set in the main .travis.yml.
+    eval $(python ./mb.py build_spec_to_shell build-spec.json)
     python --version
     pytest --pyargs ${BUILD_SPEC_PACKAGE_NAME}
 }
