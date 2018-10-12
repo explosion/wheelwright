2cc4b79bf5d97166fd52e50b03d1abacc4bd6f1d
Ines Montani <ines@ines.io>
2018-10-12 18:52:03 +0200

    Tidy up

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+# coding: utf8
+from __future__ import unicode_literals
+
 import os
 import os.path
 import sys
@@ -6,11 +9,11 @@ import json
 import subprocess
 from contextlib import contextmanager
 import time
-
 import github
 import click
 import requests
 from pathlib import Path
+
 
 LOGO = """
 ┬ ┬┬ ┬┌─┐┌─┐┬  ┬ ┬┬─┐┬┌─┐┬ ┬┌┬┐
@@ -25,20 +28,20 @@ ENV_REPO_NAME  = 'WHEELWRIGHT_REPO'
 ENV_GH_SECRET  = 'GITHUB_SECRET_TOKEN'
 
 ROOT = Path(os.environ.get(ENV_BUILD_ROOT, Path(__file__).parent))
-WHEELS_DIR = Path(os.environ.get(ENV_WHEELS_DIR, Path(__file__).parent / 'wheels'))
+WHEELS_DIR = Path(os.environ.get(ENV_WHEELS_DIR, ROOT / 'wheels'))
 SECRET_FILE = 'github-secret-token.txt'
 
 # We substitute the project name into this string to get the URL to clone:
-DEFAULT_CLONE_TEMPLATE = "https://github.com/{}.git"
+DEFAULT_CLONE_TEMPLATE = 'https://github.com/{}.git'
 
 # All the statuses we want to wait for, maps github name -> our display name
 STATUSES = {
-    "continuous-integration/appveyor/branch": "Appveyor",
-    "continuous-integration/travis-ci/push": "Travis",
+    'continuous-integration/appveyor/branch': 'Appveyor',
+    'continuous-integration/travis-ci/push': 'Travis',
 }
-NA_STATE = "n/a"
-FINAL_STATES = {"error", "failure", "success"}
-BAD_STATES = {"error", "failure"}
+NA_STATE = 'n/a'
+FINAL_STATES = {'error', 'failure', 'success'}
+BAD_STATES = {'error', 'failure'}
 STATUS_COLORS = {
     'pending': 'blue',
     'success': 'green',
@@ -53,7 +56,7 @@ STATUS_COLORS = {
 # (In particular, as of 2018-08-24, this is necessary to allow multibuild's
 # py35 to connect to github without "[SSL: TLSV1_ALERT_PROTOCOL_VERSION] tlsv1
 # alert protocol version (_ssl.c:719)" errors.)
-if sys.platform == "darwin":
+if sys.platform == 'darwin':
     import urllib3.contrib.securetransport
     urllib3.contrib.securetransport.inject_into_urllib3()
 
@@ -120,9 +123,9 @@ def _get_repo_id():
 
 
 def _do_upload(bs, paths):
-    upload_config = bs["upload-to"]
-    assert upload_config["type"] == "github-release"
-    release = get_release(upload_config["repo-id"], upload_config["release-id"])
+    upload_config = bs['upload-to']
+    assert upload_config['type'] == 'github-release'
+    release = get_release(upload_config['repo-id'], upload_config['release-id'])
     # This is a gross hack, to work around the lack of globbing on Windows
     # (see https://github.com/pallets/click/issues/1096)
     # We accept either individual files, or directories, and for directories,
@@ -130,7 +133,7 @@ def _do_upload(bs, paths):
     # recursion).
     for given_path in paths:
         if os.path.isdir(given_path):
-            subpaths = glob.glob(os.path.join(given_path, "*.whl"))
+            subpaths = glob.glob(os.path.join(given_path, '*.whl'))
         else:
             subpaths = [given_path]
         for actual_path in subpaths:
@@ -168,11 +171,7 @@ def cli():
 
 
 @cli.command(name='build-spec')
-@click.argument(
-    "build_spec",
-    type=click.Path(exists=True, dir_okay=False),
-    required=True
-)
+@click.argument('build_spec', type=click.Path(exists=True, dir_okay=False), required=True)
 def build_spec_to_shell(build_spec):
     bs = get_build_spec(build_spec)
     sys.stdout.write(
@@ -184,51 +183,45 @@ def build_spec_to_shell(build_spec):
 
 
 @cli.command(name='upload')
-@click.option(
-    "--build-spec",
-    type=click.Path(exists=True, dir_okay=False),
-    required=True
-)
-@click.argument(
-    "paths", nargs=-1, type=click.Path(exists=True)
-)
+@click.option('--build-spec', type=click.Path(exists=True, dir_okay=False), required=True)
+@click.argument('paths', nargs=-1, type=click.Path(exists=True))
 def upload(build_spec, paths):
     bs = get_build_spec(build_spec)
     _do_upload(bs, paths)
 
 
 @cli.command(name='appveyor-build')
-@click.option(
-    "--build-spec",
-    type=click.Path(exists=True, dir_okay=False),
-    required=True
-)
+@click.option('--build-spec', type=click.Path(exists=True, dir_okay=False), required=True)
 def appveyor_build(build_spec):
     bs = get_build_spec(build_spec)
-    run(["git", "clone", bs["clone-url"], "checkout"])
-    run(["pip", "install", "-Ur", "checkout\\requirements.txt"])
-    with cd("checkout"):
-        run(["git", "checkout", bs["commit"]])
-        run(["python", "setup.py", "bdist_wheel"])
-    wheels = glob.glob("checkout\\dist\\*.whl")
-    run(["pip", "install"] + wheels)
-    os.mkdir("tmp_for_test")
-    with cd("tmp_for_test"):
-        run(["pytest", "--pyargs", bs["package-name"]])
+    run(['git', 'clone', bs['clone-url'], 'checkout'])
+    run(['pip', 'install', '-Ur', 'checkout\\requirements.txt'])
+    with cd('checkout'):
+        run(['git', 'checkout', bs['commit']])
+        run(['python', 'setup.py', 'bdist_wheel'])
+    wheels = glob.glob('checkout\\dist\\*.whl')
+    run(['pip', 'install'] + wheels)
+    os.mkdir('tmp_for_test')
+    with cd('tmp_for_test'):
+        run(['pytest', '--pyargs', bs['package-name']])
     _do_upload(bs, wheels)
 
 
-@cli.command(name="build")
-@click.argument("repo", required=True)
-@click.argument("commit", required=True)
-def build(repo, commit):
+@cli.command(name='build')
+@click.argument('repo', required=True)
+@click.argument('commit', required=True)
+@click.option('--package-name')
+def build(repo, commit, package_name=None):
     """Build wheels for a given repo and commit / tag."""
     click.secho(LOGO, fg='cyan')
     repo_id = _get_repo_id()
-    user, package_name = repo.split('/', 1)
+    user, package = repo.split('/', 1)
+    if package_name is None:
+        package_name = package
     click.secho(f"Building in repo {repo_id}")
-    click.secho(f"Building wheels for {user}/{package_name}\n")
-    clone_url = DEFAULT_CLONE_TEMPLATE.format(f"{user}/{package_name}")
+    click.secho(f"Building wheels for {user}/{package}\n")
+    if clone_url is None:
+        clone_url = DEFAULT_CLONE_TEMPLATE.format(f"{user}/{package}")
     repo = get_gh().get_repo(repo_id)
 
     click.secho("Finding a unique name for this release...", fg='yellow')
@@ -237,23 +230,23 @@ def build(repo, commit):
     while True:
         release_name = "{}-{}-wheels".format(package_name, commit)
         if i > 1:
-            release_name += "-{}".format(i)
+            release_name += '-{}'.format(i)
         try:
             repo.get_release(release_name)
         except github.UnknownObjectException:
             break
         i += 1
-    branch_name = "branch-for-" + release_name
+    branch_name = 'branch-for-' + release_name
 
     bs = {
-        "clone-url": clone_url,
-        "package-name": package_name,
-        "commit": commit,
-        "upload-to": {
-            "type": "github-release",
-            "repo-id": repo_id,
-            "release-id": release_name,
-        },
+        'clone-url': clone_url,
+        'package-name': package_name,
+        'commit': commit,
+        'upload-to': {
+            'type': 'github-release',
+            'repo-id': repo_id,
+            'release-id': release_name,
+        }
     }
     bs_json = json.dumps(bs)
 
@@ -269,16 +262,16 @@ def build(repo, commit):
     # different types that are mostly *not* interchangeable:
     #   https://pygithub.readthedocs.io/en/latest/github_objects/Commit.html
     #   https://pygithub.readthedocs.io/en/latest/github_objects/GitCommit.html
-    master = repo.get_commit("master")
+    master = repo.get_commit('master')
     master_gitcommit = master.commit
     patch = github.InputGitTreeElement(
-        "build-spec.json", "100644", "blob", content=bs_json,
+        'build-spec.json', '100644', 'blob', content=bs_json,
     )
     tree = repo.create_git_tree([patch], master_gitcommit.tree)
     our_gitcommit = repo.create_git_commit(
         "Building: {}".format(release_name), tree, [master_gitcommit]
     )
-    repo.create_git_ref("refs/heads/" + branch_name, our_gitcommit.sha)
+    repo.create_git_ref('refs/heads/' + branch_name, our_gitcommit.sha)
     print(f"Commit is {our_gitcommit.sha[:8]} in branch {branch_name}.")
 
     click.secho("Waiting for build to complete...", fg='yellow')
@@ -328,8 +321,8 @@ def build(repo, commit):
         _download_release_assets(repo_id, release_name)
 
 
-@cli.command(name="download")
-@click.argument("release-id", required=True)
+@cli.command(name='download')
+@click.argument('release-id', required=True)
 def download_release_assets(release_id):
     """Download existing wheels for a release ID (name of build repo tag)."""
     click.secho(LOGO, fg='cyan')
@@ -337,6 +330,42 @@ def download_release_assets(release_id):
     click.secho(f"Downloading from repo {repo_id}")
     _download_release_assets(repo_id, release_id)
 
+
+@cli.command(name='check')
+def check():
+    """Verify that everything is set up correctly."""
+    print_ok = lambda text: click.secho(f'\u2713 {text}', fg='green')
+    print_no = lambda text: click.secho(f'\u2718 {text}', fg='red')
+
+    click.secho(LOGO, fg='cyan')
+    click.secho("Checking if things are set up correctly...\n")
+
+    # Check build repo
+    try:
+        repo_id = _get_repo_id()
+        print_ok(f"Using build repo {repo_id}")
+    except:
+        print_no(f"Couldn't get build repo name via git or {ENV_REPO_NAME} "
+                 f"env variable.")
+
+    # Check GitHub secret
+    secret_env = os.environ.get(ENV_GH_SECRET)
+    secret_path = ROOT / SECRET_FILE
+    if secret_env:
+        print_ok(f"Found GitHub secret in {ENV_GH_SECRET} environment variable.")
+    if secret_path.exists():
+        print_ok(f"Found GitHub secret in {SECRET_FILE} file.")
+    if not secret_env and not secret_path.exists():
+        print_no(f"No Github secret found in environment variable or {SECRET_FILE}.")
+
+    # Check CI files
+    token_var = 'GITHUB_SECRET_TOKEN'
+    for ci_file in ('.travis.yml', 'appveyor.yml'):
+        ci_path = ROOT / ci_file
+        if not ci_path.exists():
+            print_no(f"No {ci_file} found in root directory.")
+        else:
+            print_ok(f"{ci_file} exists in root directory.")
 
 if __name__ == "__main__":
     cli()
