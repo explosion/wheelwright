388b679bf2abfe7c82a3558e7d763ef933a13f06
Ines Montani <ines@ines.io>
2019-11-14 22:44:02 +0100

    Update run.py

--- run.py
+++ run.py
@@ -53,35 +53,37 @@ def cli():
 @cli.command(name="build")
 @click.argument("repo", required=True)
 @click.argument("commit", required=True)
-@click.option("--package-name")
-def build(repo, commit, package_name=None):
+@click.option("--package-name", help="Python package name, if different from repo")
+@click.option("--llvm", is_flag=True, help="Requires LLVM to be installed")
+def build(repo, commit, package_name=None, llvm=False):
     """Build wheels for a given repo and commit / tag."""
     print(LOGO)
     repo_id = get_repo_id()
     user, package = repo.split("/", 1)
     if package_name is None:
         package_name = package
-    msg.info("Building in repo {}".format(repo_id))
-    msg.info("Building wheels for {}/{}\n".format(user, package))
-    clone_url = DEFAULT_CLONE_TEMPLATE.format("{}/{}".format(user, package))
+    msg.info(f"Building in repo {repo_id}")
+    msg.info(f"Building wheels for {user}/{package}\n")
+    clone_url = DEFAULT_CLONE_TEMPLATE.format(f"{user}/{package}")
     repo = get_gh().get_repo(repo_id)
     with msg.loading("Finding a unique name for this release..."):
         # Pick the release_name by finding an unused one
         i = 1
         while True:
-            release_name = "{}-{}".format(package_name, commit)
+            release_name = f"{package_name}-{commit}"
             if i > 1:
-                release_name += "-{}".format(i)
+                release_name += f"-{i}"
             try:
                 repo.get_release(release_name)
             except github.UnknownObjectException:
                 break
             i += 1
-    branch_name = "branch-for-" + release_name
+    branch_name = f"branch-for-{release_name}"
     bs = {
         "clone-url": clone_url,
         "package-name": package_name,
         "commit": commit,
+        "options": {"llvm": llvm},
         "upload-to": {
             "type": "github-release",
             "repo-id": repo_id,
@@ -89,9 +91,9 @@ def build(repo, commit, package_name=None):
         },
     }
     bs_json = json.dumps(bs)
-    msg.text("Creating release {} to collect assets".format(release_name))
-    release_template = "https://github.com/{}/{}\n\n### Build spec\n\n```json\n{}\n```"
-    release_text = release_template.format(user, package, json.dumps(bs, indent=4))
+    bs_json_formatted = json.dumps(bs, indent=4)
+    msg.text(f"Creating release {release_name} to collect assets")
+    release_text = f"https://github.com/{user}/{package}\n\n### Build spec\n\n```json\n{bs_json_formatted}\n```"
     release = repo.create_git_release(release_name, release_name, release_text)
     with msg.loading("Creating build branch..."):
         # 'master' is a 'Commit'. 'master.commit' is a 'GitCommit'. These are
@@ -105,36 +107,35 @@ def build(repo, commit, package_name=None):
         )
         tree = repo.create_git_tree([patch], master_gitcommit.tree)
         our_gitcommit = repo.create_git_commit(
-            "Building: {}".format(release_name), tree, [master_gitcommit]
+            f"Building: {release_name}", tree, [master_gitcommit]
         )
-        repo.create_git_ref("refs/heads/" + branch_name, our_gitcommit.sha)
-    msg.good("Commit is {} in branch {}".format(our_gitcommit.sha[:8], branch_name))
-    checks_template = "https://github.com/{}/{}/commit/{}/checks"
-    checks = checks_template.format(user, package, our_gitcommit.sha)
-    msg.text("Release: {}".format(release.html_url))
-    msg.text("Checks:  {}".format(checks))
+        repo.create_git_ref(f"refs/heads/{branch_name}", our_gitcommit.sha)
+    msg.good(f"Commit is {our_gitcommit.sha[:8]} in branch {branch_name}")
+    checks = f"https://github.com/{user}/{package}/commit/{our_gitcommit.sha}/checks"
+    msg.text(f"Release: {release.html_url}")
+    msg.text(f"Checks:  {checks}")
 
 
 @cli.command(name="download")
-@click.argument("release-id", required=True)
+@click.argument("release-id")
 def download_release_assets(release_id):
     """Download existing wheels for a release ID (name of build repo tag)."""
     print(LOGO)
     repo_id = get_repo_id()
-    msg.info("Downloading from repo {}".format(repo_id))
+    msg.info(f"Downloading from repo {repo_id}")
     download_path = WHEELS_DIR / release_id
-    msg.info("Downloading to {}/...".format(download_path))
+    msg.info(f"Downloading to {download_path}/...")
     if not download_path.exists():
         download_path.mkdir(parents=True)
     with requests.Session() as s:
         release = get_release(repo_id, release_id)
         for asset in release.get_assets():
-            print("  - " + asset.name)
+            print(f"  - {asset.name}")
             save_path = download_path / asset.name
             r = s.get(asset.browser_download_url)
             with save_path.open("wb") as f:
                 f.write(r.content)
-    msg.good("All done!", "See {}/ for your wheels.".format(download_path))
+    msg.good("All done!", f"See {download_path}/ for your wheels.")
 
 
 def get_gh():
@@ -145,8 +146,8 @@ def get_gh():
         with token_path.open("r", encoding="utf-8") as f:
             token = f.read().strip()
     else:
-        err = "Can't find Github token (checked {} envvar and {}"
-        msg.fail(err.format(ENV.GH_SECRET, token_path), exits=1)
+        err = f"Can't find GitGub token. Not {ENV.GH_SECRET} envvar or in {token_path}"
+        msg.fail(err, exits=1)
     return github.Github(token)
 
 
@@ -156,7 +157,7 @@ def get_release(repo_id, release_id):
     # https://pygithub.readthedocs.io/en/latest/github_objects/GitRelease.html
     release = repo.get_release(release_id)
     if release is None:
-        msg.fail("Release not found: {}".format(release_id), exits=1)
+        msg.fail(f"Release not found: {release_id}", exits=1)
     return release
 
 
@@ -177,10 +178,10 @@ def get_repo_id():
     except subprocess.CalledProcessError:
         pass
     msg.fail(
-        "Error: Not a valid repository: {}.".format(Path.cwd()),
-        "Make sure you're in the build repo directory or use the "
-        "{} environment variable to specify the <user>/<repo> "
-        "build repository.".format(ENV.REPO_NAME),
+        f"Error: Not a valid repository: {Path.cwd()}.",
+        f"Make sure you're in the build repo directory or use the "
+        f"{ENV.REPO_NAME} environment variable to specify the <user>/<repo> "
+        f"build repository.",
         exits=1,
     )
 
